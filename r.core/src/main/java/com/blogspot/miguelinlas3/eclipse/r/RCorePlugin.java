db1cc1fa028c803a45eeaf61611aab18d29522f7
Miguel Pastor <miguelinlas3@gmail.com>
2011-10-12 20:18:05 +0200

    Activator class to control de plug-in lifecycle

---
+++ r.core/src/main/java/com/blogspot/miguelinlas3/eclipse/r/RCorePlugin.java
@@ -0,0 +1,57 @@
+package com.blogspot.miguelinlas3.eclipse.r;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class RCorePlugin extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "r.core";
+
+	// The shared instance
+	private static RCorePlugin plugin;
+
+
+	/**
+	 * The constructor
+	 */
+	public RCorePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static RCorePlugin getDefault() {
+		return plugin;
+	}
+
+}
