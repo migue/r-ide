9620fdc90cb1632d19cbb5859574721876547ad4
Miguel Pastor <miguelinlas3@gmail.com>
2011-10-12 20:34:29 +0200

    Including R Nature

---
+++ r.core/src/main/java/com/blogspot/miguelinlas3/r/eclipse/nature/RNature.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.blogspot.miguelinlas3.r.eclipse.nature;
+
+import org.eclipse.dltk.core.ScriptNature;
+
+import com.blogspot.miguelinlas3.eclipse.r.RCorePlugin;
+
+/**
+ * @author Administrador
+ * 
+ */
+public class RNature extends ScriptNature {
+
+	/**
+	 * Identificador de la <code>nature</code> R
+	 */
+	public static final String R_NATURE_ID = RCorePlugin.PLUGIN_ID + ".nature";
+
+}
