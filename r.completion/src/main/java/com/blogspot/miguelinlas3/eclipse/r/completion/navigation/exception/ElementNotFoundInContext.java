df206b9b08fb2ebf8839b780881c0277e41735c6
Miguel Pastor <miguelinlas3@gmail.com>
2011-10-13 23:29:57 +0200

    Not found element (in current context)

---
+++ r.completion/src/main/java/com/blogspot/miguelinlas3/eclipse/r/completion/navigation/exception/ElementNotFoundInContext.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.blogspot.miguelinlas3.eclipse.r.completion.navigation.exception;
+
+/**
+ * @author migue
+ * 
+ */
+public class ElementNotFoundInContext extends Exception {
+
+	public ElementNotFoundInContext(String message) {
+		super(message);
+	}
+}
